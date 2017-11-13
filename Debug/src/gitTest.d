2d763c01c1c0cdf89989ba082b1c0a194d9e8d2b
kociszz <kocisz93@gmail.com>
2017-11-13 11:50:49 +0100

    pierwsze_testy

---
+++ Debug/src/gitTest.d
@@ -0,0 +1,1 @@
+src/gitTest.o: ../src/gitTest.c
---
+++ src/gitTest.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : gitTest.c
+ Author      : Kociszewski
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Git test!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
